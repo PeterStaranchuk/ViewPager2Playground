620ad92ca1e716537518a630ed8cc6c7a0ce6e57
Petr Staranchuk <petr.staranchuk@gmail.com>
2019-11-28 10:55:33 +0300

    Add <layout> tag to activity_main.xml to use binding in activity Add adapter initialization

--- app/src/main/java/com/peterstaranchuk/viewpager2playground/MainActivity.kt
+++ app/src/main/java/com/peterstaranchuk/viewpager2playground/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.peterstaranchuk.viewpager2playground
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.peterstaranchuk.viewpager2playground.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewPager2.adapter = ItemsAdapter(ItemSource().getItems())
     }
 }
